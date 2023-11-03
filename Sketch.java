1e53140af3a48ef73a638e3153a885b20d336c5d
Julian3377 <julianli3377@gmail.com>
2023-11-02 21:24:24 -0400

    Added drawings

--- Sketch.java
+++ Sketch.java
@@ -2,36 +2,16 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(1200, 600);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(45, 150, 207);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    /*
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-*/
     draw_section_outlines();
     draw_section1();
     draw_section2();
@@ -45,7 +25,6 @@ public class Sketch extends PApplet {
 
     
   }
-
 
   /**
    * Draw the outlines for all sections
@@ -67,17 +46,15 @@ public class Sketch extends PApplet {
     rect(900, 0, 300, 300);
   }
   
-  /**
-   * draws the bottom left section
-   */
   public void draw_section1(){
     int intX = 0;
     int intY = 0;
 
-    for(int intRow = 0; intRow < 30; intRow++){
-      for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+    // Fills section with squares
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 3 + intRow; 
+        intY = 300 + 3 + intColumn;
 
         fill(255);
         noStroke();
@@ -87,51 +64,160 @@ public class Sketch extends PApplet {
     }
   }
 
-  /**
-   * Use the modulus operator and an if statement to select the color
-   * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
-   */
   public void draw_section2(){
+    int intX = 0;
+    int intY = 0;
+    
+    // Fills sections with squares, but alternates between black-white based on the row
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 300 + 3 + intRow;  
+        intY = 300 + 3 + intColumn;
 
+        if (intRow % 4 == 0) {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        } else {
+            fill(0);
+            noStroke();
+            rect(intX, intY, 5, 5);
+        }
+      }
+    }
   }
 
-  /**
-   * Use the modulus operator and an if/else statement to select the color.
-   * Don't use multiple 'if' statements.
-   */
   public void draw_section3(){
+    int intX = 0;
+    int intY = 0;
+    boolean isBlack = true;
 
+    // Fills sections with sqaures, but alternates between black-white for each loop 
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 300 + 300 + 3 + intRow;  
+        intY = 300 + 3 + intColumn;
+
+        if (isBlack) {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+          isBlack = false;
+        } else {
+            fill(255);
+            noStroke();
+            rect(intX, intY, 5, 5);
+            isBlack = true;
+        }
+      }
+    }
   }
 
-  /**
-   * Use the modulus operator and just one 'if' statement to select the color.
-   */
   public void draw_section4(){
 
+    int intX = 0;
+    int intY = 0;
+    boolean isBlack = true;
+
+    // Fills section with squares, but if the row is even, it alternates between white-black
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      boolean blackRow = intRow % 20 == 10;
+      for(int intColumn = 0; intColumn < 300; intColumn += 10){
+        intX = 300 + 300 + 300 + 3 + intRow;  
+        intY = 300 + 3 + intColumn;
+
+        if (blackRow) {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        } else {
+            if (isBlack) {
+              fill(0);
+              noStroke();
+              rect(intX, intY, 5, 5);
+              isBlack = false;
+            }
+            
+            else {
+              fill(255);
+              noStroke();
+              rect(intX, intY, 5, 5);
+              isBlack = true;
+            }
+
+        }
+
+      }
+    }
+
   }
 
-  /**
-   * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
-   */
   public void draw_section5(){
+    int intX = 0;
+    int intY = 0;
 
+    // Fills the bottom-right corner of the square
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = 300 - intRow; intColumn < 300; intColumn += 10){
+        intX = 3 + intRow; 
+        intY = 3 + intColumn;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
 
   public void draw_section6(){
+    int intX = 0;
+    int intY = 0;
 
+    // Fills the bottom-left of the square
+    for(int intRow = 0; intRow < 300; intRow += 10){
+      for(int intColumn = intRow; intColumn < 300; intColumn += 10){
+        intX = 3 + 300 + intRow; 
+        intY = 3 + intColumn;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
 
   public void draw_section7(){
+    int intX = 0;
+    int intY = 0;
 
+    // Fills the top-left of the square, reversing intY
+    for(int intColumn = 300; intColumn >= 0; intColumn -= 10){
+      for(int intRow = 0; intRow < intColumn; intRow += 10){
+        intX = 3 + 300 + 300 + intRow; 
+        intY = 3 + 300 - intColumn; 
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
   
   public void draw_section8(){
+    int intX = 0;
+    int intY = 0;
 
+    // Fills the top-right of the square, reversing the column/row loops
+    for(int intColumn = 0; intColumn < 300; intColumn += 10){
+      for(int intRow = intColumn; intRow < 300; intRow += 10){
+        intX = 3 + 300 + 300 + 300 + intRow; 
+        intY = 3 + intColumn;
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
-
-
-
-
-
 
 }
